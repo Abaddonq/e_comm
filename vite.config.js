a9e3bcc8ea9b488a62171e87cea8a163ee35dd47
abaddonq <sefasahin1762@gmail.com>
2026-03-04 15:02:34 +0300

    Optimize performance

--- vite.config.js
+++ vite.config.js
@@ -13,20 +13,6 @@ export default defineConfig({
         }),
     ],
     build: {
-        minify: 'terser',
-        terserOptions: {
-            compress: {
-                drop_console: true,
-                drop_debugger: true,
-            },
-        },
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    vendor: ['jquery'],
-                },
-            },
-        },
-        cssCodeSplit: true,
+        minify: 'esbuild',
     },
 });
